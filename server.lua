cfd06e6f01d78998a450300d384698dd307cfdbe
Trinket Entertainment <damienostler1@outlook.com>
2018-05-03 10:44:44 -0400

    Initla Files Upload

---
+++ server.lua
@@ -0,0 +1,544 @@
+
+robbableSpots = {
+	["Little Soul 24/7 Register #1"]={name="Little Soul 24/7 Register #1",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Legion Flecca Bank Vault",
+	},
+
+	["Little Soul 24/7 Register #2"]={name="Little Soul 24/7 Register #2",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Legion Flecca Bank Vault",
+	},
+
+	["Little Soul 24/7 Safe"]={name="Little Soul 24/7 Safe",
+	currentMoney=2000,
+	maxMoney=25000,
+	moneyRengerationRate=350, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Legion Flecca Bank Vault",
+	},
+
+	["Algonquin 24/7 Register"]={name="Algonquin 24/7 Register",
+	currentMoney=10000,
+	maxMoney=10000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Route 68 Flecca Bank Vault",
+	},
+
+	["Mirror Park 24/7 Register #1"]={name="Mirror Park 24/7 Register #1",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="East Hawick Flecca Bank Vault",
+	},
+	["Mirror Park 24/7 Register #2"]={name="Mirror Park 24/7 Register #2",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="East Hawick Flecca Bank Vault",
+	},
+	["Mirror Park 24/7 Safe"]={name="Mirror Park 24/7 Safe",
+	currentMoney=2000,
+	maxMoney=25000,
+	moneyRengerationRate=350, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="East Hawick Flecca Bank Vault",
+	},
+
+	["Downtown Vinewood 24/7 Register #1"]={name="Downtown Vinewood 24/7 Register #1",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Pacific Standard Bank Vault",
+	},
+	["Downtown Vinewood 24/7 Register #2"]={name="Downtown Vinewood 24/7 Register #2",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Pacific Standard Bank Vault",
+	},
+	["Downtown Vinewood 24/7 Safe"]={name="Downtown Vinewood 24/7 Safe",
+	currentMoney=2000,
+	maxMoney=25000,
+	moneyRengerationRate=350, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Pacific Standard Bank Vault",
+	},
+
+	["Rockford Dr 24/7 Register #1"]={name="Rockford Dr 24/7 Register #1",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Grean Ocean Hwy Flecca Bank Vault",
+	},
+	["Rockford Dr 24/7 Register #2"]={name="Rockford Dr 24/7 Register #2",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Grean Ocean Hwy Flecca Bank Vault",
+	},
+	["Rockford Dr 24/7 Safe"]={name="Rockford Dr 24/7 Safe",
+	currentMoney=2000,
+	maxMoney=25000,
+	moneyRengerationRate=350, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Grean Ocean Hwy Flecca Bank Vault",
+	},
+
+	["South Senora Fwy 24/7 Register #1"]={name="South Senora Fwy 24/7 Register #1",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Route 68 Flecca Bank Vault",
+	},
+	["South Senora Fwy 24/7 Register #2"]={name="South Senora Fwy 24/7 Register #2",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Route 68 Flecca Bank Vault",
+	},
+	["South Senora Fwy 24/7 Safe"]={name="South Senora Fwy 24/7 Safe",
+	currentMoney=2000,
+	maxMoney=25000,
+	moneyRengerationRate=350, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Route 68 Flecca Bank Vault",
+	},
+
+	["North Senora Fwy 24/7 Register #1"]={name="North Senora Fwy 24/7 Register #1",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Blaine County Savings Vault",
+	},
+	["North Senora Fwy 24/7 Register #2"]={name="North Senora Fwy 24/7 Register #2",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Blaine County Savings Vault",
+	},
+	["North Senora Fwy 24/7 Safe"]={name="North Senora Fwy 24/7 Safe",
+	currentMoney=2000,
+	maxMoney=25000,
+	moneyRengerationRate=350, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Blaine County Savings Vault",
+	},
+
+	["Route 68 24/7 Register #1"]={name="Route 68 24/7 Register #1",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Grean Ocean ",
+	},
+	["Route 68 24/7 Register #2"]={name="Route 68 24/7 Register #2",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Grean Ocean ",
+	},
+	["Route 68 24/7 Safe"]={name="Route 68 24/7 Safe",
+	currentMoney=2000,
+	maxMoney=25000,
+	moneyRengerationRate=350, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Grean Ocean ",
+	},
+
+	["Innocence Blvd 24/7 Register #1"]={name="Innocence Blvd 24/7 Register #1",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Legion Flecca Bank Vault",
+	},
+	["Innocence Blvd 24/7 Register #2"]={name="Innocence Blvd 24/7 Register #2",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Legion Flecca Bank Vault",
+	},
+	["Innocence Blvd 24/7 Safe"]={name="Innocence Blvd 24/7 Safe",
+	currentMoney=2000,
+	maxMoney=25000,
+	moneyRengerationRate=350, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Legion Flecca Bank Vault",
+	},
+
+
+	--CLUBS
+
+	["Bahama Mamas Cash Register #1"]={name="Bahama Mamas Cash Register #1",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="West Hawick Flecca Bank Vault",
+	},
+	["Bahama Mamas Cash Register #2"]={name="Bahama Mamas Cash Register #2",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="West Hawick Flecca Bank Vault",
+	},
+	["Bahama Mamas Cash Register #3"]={name="Bahama Mamas Cash Register #3",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="West Hawick Flecca Bank Vault",
+	},
+	["Bahama Mamas Cash Register #4"]={name="Bahama Mamas Cash Register #4",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="West Hawick Flecca Bank Vault",
+	},
+	["Bahama Mamas Cash Register #5"]={name="Bahama Mamas Cash Register #5",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="West Hawick Flecca Bank Vault",
+	},
+	["Bahama Mamas Cash Register #6"]={name="Bahama Mamas Cash Register #6",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="West Hawick Flecca Bank Vault",
+	},
+
+	["Tequilala Register"]={name="Tequilala Register",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Pacific Standard Bank Vault",
+	},
+	["Tequilala Safe"]={name="Tequilala Safe",
+	currentMoney=3000,
+	maxMoney=15000,
+	moneyRengerationRate=250, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Pacific Standard Bank Vault",
+	},
+
+
+
+
+
+
+
+
+
+--- LIQUIOR STORES
+
+
+
+	["Prosperity Liquior Store Register"]={name="Prosperity Liquior Store Register",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=200, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="West Hawick Flecca Bank Vault",
+	},
+	["Prosperity Liquior Store Safe"]={name="Prosperity Liquior Store Safe",
+	currentMoney=2000,
+	maxMoney=30000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="West Hawick Flecca Bank Vault",
+	},
+
+	["El Rancho Blvd Liquior Store Register"]={name="El Rancho Blvd Liquior Store Register",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=200, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Legion Flecca Bank Vault",
+	},
+	["El Rancho Blvd Liquior Store Safe"]={name="El Rancho Blvd Liquior Store Safe",
+	currentMoney=2000,
+	maxMoney=30000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Legion Flecca Bank Vault",
+	},
+
+	["Great Ocean Hwy Liquior Store Register"]={name="Great Ocean Hwy Liquior Store Register",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=200, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Great Owean Hwy Flecca Bank Vault",
+	},
+	["Great Ocean Hwy Liquior Store Safe"]={name="Great Ocean Hwy Liquior Store Safe",
+	currentMoney=2000,
+	maxMoney=30000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Great Owean Hwy Flecca Bank Vault",
+	},
+
+	["Route 68 Liquior Store Register"]={name="Route 68 Liquior Store Register",
+	currentMoney=500,
+	maxMoney=5000,
+	moneyRengerationRate=200, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Route 68 Flecca Bank Vault",
+	},
+	["Route 68 Liquior Store Safe"]={name="Route 68 Liquior Store Safe",
+	currentMoney=2000,
+	maxMoney=30000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=true,
+	isBank=false,
+	bankToDeliverToo="Route 68 Flecca Bank Vault",
+	},
+
+
+
+
+
+
+
+-- bank booths
+
+	["Pacific Standard Bank Booth #1"]={name="Pacific Standard Bank Booth #1",
+	currentMoney=250,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+	["Pacific Standard Bank Booth #2"]={name="Pacific Standard Bank Booth #2",
+	currentMoney=250,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+	["Pacific Standard Bank Booth #3"]={name="Pacific Standard Bank Booth #3",
+	currentMoney=250,
+	maxMoney=5000,
+	moneyRengerationRate=100, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+
+
+
+
+
+	-- BANKS
+
+
+
+	["East Hawick Flecca Bank Vault"]={name="East Hawick Flecca Bank Vault",
+	currentMoney=10000,
+	maxMoney=200000,
+	moneyRengerationRate=400, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+	["East Hawick Flecca Bank Vault"]={name="East Hawick Flecca Bank Vault",
+	currentMoney=10000,
+	maxMoney=200000,
+	moneyRengerationRate=400, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+	["Route 68 Flecca Bank Vault"]={name="Route 68 Flecca Bank Vault",
+	currentMoney=10000,
+	maxMoney=200000,
+	moneyRengerationRate=400, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+	["Hawick Flecca Bank Vault"]={name="Hawick Flecca Bank Vault",
+	currentMoney=10000,
+	maxMoney=200000,
+	moneyRengerationRate=400, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+	["West Hawick Flecca Bank Vault"]={name="West Hawick Flecca Bank Vault",
+	currentMoney=10000,
+	maxMoney=200000,
+	moneyRengerationRate=400, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+	["Legion Flecca Bank Vault"]={name="Legion Flecca Bank Vault",
+	currentMoney=10000,
+	maxMoney=200000,
+	moneyRengerationRate=400, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+	["Great Ocean Hwy Flecca Bank Vault"]={name="Great Ocean Hwy Flecca Bank Vault",
+	currentMoney=10000,
+	maxMoney=200000,
+	moneyRengerationRate=400, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+	["Pacific Standard Bank Vault"]={name="Pacific Standard Bank Vault",
+	currentMoney=10000,
+	maxMoney=200000,
+	moneyRengerationRate=400, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+
+	["Blaine County Savings Vault"]={name="Blaine County Savings Vault",
+	currentMoney=10000,
+	maxMoney=200000,
+	moneyRengerationRate=400, -- Per Minute
+	takesMoneyToBankOnMax=false,
+	isBank=true,
+	bankToDeliverToo="None",
+	},
+}
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(60000)
+		for name,spot in pairs(robbableSpots) do
+			if(spot.currentMoney<spot.maxMoney)then
+				spot.currentMoney = spot.currentMoney + spot.moneyRengerationRate
+			end
+		end
+	end
+end)
+
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1800000)
+		for name,spot in pairs(robbableSpots)do
+			if(spot.isBank==false)then
+				local aFourth = spot.currentMoney/4
+				local randomAmount = math.random(aFourth,aFourth*3)
+				spot.currentMoney = spot.currentMoney-randomAmount
+				robbableSpots[spot.bankToDeliverToo].currentMoney=robbableSpots[spot.bankToDeliverToo].currentMoney+randomAmount
+				if(robbableSpots[spot.bankToDeliverToo].currentMoney>robbableSpots[spot.bankToDeliverToo].maxMoney)then
+					robbableSpots[spot.bankToDeliverToo].currentMoney=robbableSpots[spot.bankToDeliverToo].maxMoney
+				end
+			end
+		end
+	end
+end)
+
+RegisterServerEvent("robberies:robberyOver")
+AddEventHandler("robberies:robberyOver", function(name) 	
+	TriggerEvent('es:getPlayerFromId', source, function(target) 
+		if(target)then
+			target.addMoney(robbableSpots[name].currentMoney) 
+			robbableSpots[name].currentMoney=0
+		end
+	end)
+end)
+
+RegisterServerEvent("robberies:syncSpots")
+AddEventHandler("robberies:syncSpots", function(spots)
+	TriggerClientEvent("robberies:syncSpotsClient", -1, spots)
+end)
+
+RegisterServerEvent("robberies:policeCheck")
+AddEventHandler("robberies:policeCheck", function()
+	TriggerEvent('f:getPlayers', function(players)
+		local nbPolice = 0
+		for i,p in pairs(players) do
+			if p.getSessionVar('policeInService') == true then
+				nbPolice = nbPolice + 1
+			end
+		end
+		TriggerClientEvent("robberies:StartRobbery", source, nbPolice)
+	end)
+end)
